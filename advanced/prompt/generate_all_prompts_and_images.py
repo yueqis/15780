34b23ed50f66af92b18cef4f7497a912558aafcb
Kathryn-cat <jinqic@alumni.cmu.edu>
2025-05-02 15:12:20 -0400

    dev

--- advanced/prompt/generate_all_prompts_and_images.py
+++ advanced/prompt/generate_all_prompts_and_images.py
@@ -1,3 +1,4 @@
+import json
 import os
 from datasets import load_dataset
 
@@ -10,6 +11,19 @@ code = ds["code"]
 
 sorted_list = sorted(zip(problem_ids, names, code), key=lambda x: x[0])
 problem_ids, names, code = zip(*sorted_list)
+
+with open("../../retrieval_dataset.json", "r", encoding="latin1") as f:
+    rag_data = json.load(f)
+
+task_ids, docs, image_urls = [], [], []
+for item in rag_data:
+    task_ids.append(int(item["task_id"]))
+    docs.append(item["doc"])
+    image_urls.append(item["image_urls"])
+
+sorted_rag_data = sorted(zip(task_ids, docs, image_urls), key=lambda x: x[0])
+task_ids, docs, image_urls = zip(*sorted_rag_data)
+
 
 PROBLEM_STATEMENT = """You write custom CUDA kernels to replace the pytorch operators in the given architecture to get speedups. \n
     You have complete freedom to choose the set of operators you want to replace. You may make the decision to replace some operators with custom CUDA kernels and leave others unchanged. You may replace multiple operators with custom implementations, consider operator fusion opportunities (combining multiple operators into a single kernel, for example, combining matmul+relu), or algorithmic changes (such as online softmax). You are only limited by your imagination.\n
@@ -61,7 +75,7 @@ def read_file(file_path) -> str:
 example_new_arch_src = read_file(os.path.join(os.getcwd(), "example_new_arch_src.py"))
 
 
-def prompt_generate_custom_cuda(arc_src: str) -> str:
+def prompt_generate_custom_cuda(arc_src: str, nv_doc: str, urls: str) -> str:
     prompt = PROBLEM_STATEMENT
 
     prompt += f"""
@@ -75,6 +89,21 @@ def prompt_generate_custom_cuda(arc_src: str) -> str:
     ``` \n
     """
 
+    # retrieval augmented
+    prompt += f"""Here are some relevant GPU kernel optimization documents and CUDA domain knowledge that you may use to assist you in writing the kernel:
+    \n
+    {nv_doc}
+    \n
+    """
+
+    prompt += f"""Here are some images in the form of urls illustrating the CUDA programming essentials and help you understand more about GPU optimization principles:\n"""
+
+    for url in urls:
+        prompt += f"""
+        {url}
+        \n
+        """
+
     prompt += f"""
     You are given the following architecture: \n
     ```
@@ -87,10 +116,10 @@ def prompt_generate_custom_cuda(arc_src: str) -> str:
 
 for i in range(100):
     try:
-        prompt = prompt_generate_custom_cuda(code[i])
+        prompt = prompt_generate_custom_cuda(code[i], docs[i], image_urls[i])
         file_name = f"prompt_{i+1}.txt"
 
-        with open(file_name, "w", encoding="utf-8") as f:
+        with open(file_name, "w", encoding="latin1") as f:
             f.write(prompt)
 
         print(f"Successfully generated {file_name}")
